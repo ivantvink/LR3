43292f5425366473d89aa5d94169fc9ebc16ab69
IvanTvink <ivangoncharov@tutamail.com>
2024-09-02 20:53:01 +0300

    Add clipboard message and logic of that

---
+++ src/utils/ClipboardUtils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import javax.swing.*;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+
+public class ClipboardUtils {
+    // Метод для копирования текста в буфер обмена с выводом сообщения
+    public static void copyToClipboard(String str) {
+        StringSelection selection = new StringSelection(str);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+        JOptionPane.showMessageDialog(null, "Password copied to clipboard!");
+    }
+}
