49b7d85b5e4621007113b93b7d9df9b7e9a0c087
Ivantwink2 <ivangoncharov286@mailfence.com>
2024-09-02 19:42:53 +0300

    Now checked symbols must be used

--- src/logic/PasswordGenerator.java
+++ src/logic/PasswordGenerator.java
@@ -1,6 +1,9 @@
 package logic;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PasswordGenerator {
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
@@ -8,27 +11,50 @@ public class PasswordGenerator {
     private static final String DIGITS = "0123456789";
     private static final String SYMBOLS = "!@#$%^&*()_+-=[]{}|;':,.<>?";
 
-    // Метод для генерации пароля
     public static String generate(int length, boolean useNumbers, boolean useUppercase, boolean useSymbols) {
         StringBuilder characterPool = new StringBuilder(LOWERCASE);
         SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
 
-        // Добавляем выбранные типы символов в пул символов
-        if (useNumbers) characterPool.append(DIGITS);
-        if (useUppercase) characterPool.append(UPPERCASE);
-        if (useSymbols) characterPool.append(SYMBOLS);
+        // Сначала добавляем по одному символу из каждого выбранного типа
+        if (useNumbers) {
+            password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+            characterPool.append(DIGITS);
+        }
+        if (useUppercase) {
+            password.append(UPPERCASE.charAt(random.nextInt(UPPERCASE.length())));
+            characterPool.append(UPPERCASE);
+        }
+        if (useSymbols) {
+            password.append(SYMBOLS.charAt(random.nextInt(SYMBOLS.length())));
+            characterPool.append(SYMBOLS);
+        }
 
-        // Если пользователь не выбрал ни одного типа символов, выбрасываем исключение
-        if (characterPool.isEmpty()) {
+        // Если пользователь не выбрал ни одного типа символов
+        if (characterPool.length() == LOWERCASE.length()) {
             throw new IllegalArgumentException("No character types selected. Please select at least one option.");
         }
 
-        // Генерация пароля из выбранного пула символов
-        StringBuilder password = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
+        // Генерируем оставшуюся часть пароля
+        for (int i = password.length(); i < length; i++) {
             password.append(characterPool.charAt(random.nextInt(characterPool.length())));
         }
 
-        return password.toString();
+        // Перемешиваем символы, чтобы гарантировать случайность
+        return shuffleString(password.toString());
+    }
+
+    // Метод для перемешивания символов в строке
+    private static String shuffleString(String input) {
+        List<Character> characters = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            characters.add(c);
+        }
+        Collections.shuffle(characters);
+        StringBuilder shuffled = new StringBuilder();
+        for (char c : characters) {
+            shuffled.append(c);
+        }
+        return shuffled.toString();
     }
 }
